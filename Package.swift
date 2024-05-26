0e1ba576aee8b8fcd231814ca41c7741988cc2db
Amin-0-1 <m.amin@moddakir.com>
2024-05-26 12:28:16 +0300

    initial commit

---
+++ Package.swift
@@ -0,0 +1,45 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "ModdakirSDK",
+    platforms: [.iOS(.v13)],
+    products: [
+        .library(
+            name: "ModdakirSDK",
+            targets: ["ModdakirSDKWrapper"]
+        ),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/Amin-0-1/NetworkManager", .upToNextMajor(from: "1.3.1")),
+        .package(url: "https://github.com/AgoraIO/AgoraRtm_iOS", .upToNextMajor(from: "1.5.1")),
+        .package(url: "https://github.com/AgoraIO/AgoraAudio_iOS", .upToNextMajor(from: "4.3.1"))
+    ],
+    
+    targets: [
+        .target(
+            name: "ModdakirSDKWrapper",
+            dependencies: [
+                .target(name: "ModdakirSDK"),
+                .product(name: "NetworkManager", package: "NetworkManager"),
+                .product(name: "AgoraRtmKit", package: "AgoraRtm_iOS"),
+                .product(name: "RtcBasic", package: "AgoraAudio_iOS"),
+            ],
+            path: "./Sources/ModdakirSDKWrapper",
+            linkerSettings: [
+                .linkedFramework("ModdakirSDK")
+            ]
+        ),
+        .binaryTarget(
+            name: "ModdakirSDK",
+            url: "https://github.com/Moddakir-App/ModdakirSDK/releases/download/v0.0.1/ModdakirSDK.xcframework.zip",
+            checksum: "fbd9487b2d92e5cdcfd895bf475eccb1b6ef6e6e44495f4e9ef7cc5f69361605"
+        )
+//        .binaryTarget(
+//            name: "ModdakirSDK",
+//            path: "./XCFrameworks/ModdakirSDK.xcframework"
+//        )
+    ]
+)
---
+++ Sources/ModdakirSDKWrapper/ModdakirSDKWrapper.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Amin on 5/23/24.
+//
+
+import Foundation
