831f1bd06a518bebc549d6695b3bcd945d475949
Amin-0-1 <m.amin@moddakir.com>
2024-05-30 17:52:04 +0300

    bump 0.0.11

--- Package.swift
+++ Package.swift
@@ -15,7 +15,8 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/Amin-0-1/NetworkManager", .upToNextMajor(from: "1.4.0")),
         .package(url: "https://github.com/AgoraIO/AgoraRtm_iOS", .upToNextMajor(from: "1.5.1")),
-        .package(url: "https://github.com/AgoraIO/AgoraAudio_iOS", .upToNextMajor(from: "4.3.1"))
+        .package(url: "https://github.com/AgoraIO/AgoraAudio_iOS", .upToNextMajor(from: "4.3.1")),
+        .package(url: "https://github.com/alexiscreuzot/SwiftyGif", .upToNextMajor(from: "5.4.5"))
     ],
     
     targets: [
@@ -26,13 +27,15 @@ let package = Package(
                 .product(name: "NetworkManager", package: "NetworkManager"),
                 .product(name: "AgoraRtmKit", package: "AgoraRtm_iOS"),
                 .product(name: "RtcBasic", package: "AgoraAudio_iOS"),
+                .product(name: "SwiftyGif", package: "SwiftyGif")
+                
             ],
             path: "./Sources/ModdakirSDKWrapper"
         ),
         
         .binaryTarget(
             name: "ModdakirSDK",
-            url: "https://github.com/Moddakir-App/ModdakirSDK/releases/download/0.0.10/ModdakirSDK.xcframework.zip",
+            url: "https://github.com/Moddakir-App/ModdakirSDK/releases/download/0.0.11/ModdakirSDK.xcframework.zip",
             checksum: "1262f091c2ad66957a4676dfbc36bbe17ea3e3183470a40b0f3d467580934059"
         )
     ]
