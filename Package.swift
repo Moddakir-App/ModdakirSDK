ebd3874cf75aa846396c6a8e5ba54dcf412844ae
Amin-0-1 <m.amin@moddakir.com>
2025-05-07 21:51:38 +0300

    bump to 0.6.1

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/Amin-0-1/NetworkManager", .upToNextMajor(from: "1.4.0")),
+        .package(url: "https://github.com/Amin-0-1/NetworkManager", .upToNextMajor(from: "1.6.2")),
         .package(url: "https://github.com/AgoraIO/AgoraAudio_iOS", .upToNextMajor(from: "4.3.1")),
         .package(url: "https://github.com/alexiscreuzot/SwiftyGif", .upToNextMajor(from: "5.4.5"))
     ],
@@ -33,8 +33,8 @@ let package = Package(
         
         .binaryTarget(
             name: "ModdakirSDK",
-            url: "https://github.com/Moddakir-App/ModdakirSDK/releases/download/0.1.5/ModdakirSDK.xcframework.zip",
-            checksum: "049994d9d29742424062660aee18fb54834b5ef840e5fae45436c9dfbe9db348"
+            url: "https://github.com/Moddakir-App/ModdakirSDK/releases/download/0.1.6/ModdakirSDK.xcframework.zip",
+            checksum: "4347d66f4c5e2ae3bf6c21f69f78a0caa59a1698a5598f9af90c985b18cfd6ff"
         )
     ]
 )
